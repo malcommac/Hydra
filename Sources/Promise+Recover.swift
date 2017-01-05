6a23a39a89de5bbc91645abab80dc3a777395b6a
daniele margutti <hello@danielemargutti.com>
2017-01-05 18:15:34 +0100

    Added .recover()

---
+++ Sources/Promise+Recover.swift
@@ -0,0 +1,29 @@
+//
+//  Promise+Recover.swift
+//  Hydra
+//
+//  Created by Daniele Margutti on 05/01/2017.
+//  Copyright © 2017 Daniele Margutti. All rights reserved.
+//
+
+import Foundation
+
+public extension Promise {
+	
+	/// Allows Promise recovery by returning another Promise
+	///
+	/// - Parameter recovery: recovery handler which should return a new promise
+	/// - Returns: a new promise to append to the chain
+	public func recover(_ recovery: @escaping (Error) throws -> Promise<R>) -> Promise<R> {
+		return Promise<R>(self.context, { resolve, reject in
+			self.then(self.context, resolve).catch(context: self.context, { error in
+				do {
+					try recovery(error).then(self.context, resolve)
+				} catch (let error) {
+					reject(error)
+				}
+			})
+		})
+	}
+	
+}
