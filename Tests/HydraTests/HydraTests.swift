6c7116f1306d5b661ce98d3acc0cdc69b8283a6d
daniele margutti <hello@danielemargutti.com>
2017-01-08 11:23:16 +0100

    Tests for resolved and rejected Promise

---
+++ Tests/HydraTests/HydraTests.swift
@@ -0,0 +1,24 @@
+//
+//  HydraTests.swift
+//  HydraTests
+//
+//  Created by Daniele Margutti on 05/01/2017.
+//  Copyright © 2017 Daniele Margutti. All rights reserved.
+//
+
+import XCTest
+@testable import Hydra
+
+class HydraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+		
+	    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
